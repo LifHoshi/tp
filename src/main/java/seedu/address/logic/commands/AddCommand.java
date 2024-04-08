bd627cb5d3abe3c10052e1b3ebca6c4f818a5e76
LifHoshi <e0907413@u.nus.edu>
2024-04-08 22:06:34 +0800

    Fix the add command issue

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -28,14 +28,15 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
-            + "[" + PREFIX_ROLE + "ROLE]...\n"
+            + PREFIX_ROLE + "ROLE "
+            + PREFIX_METADATA + "METADATA "
+            + PREFIX_CCA + "CCA\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_ROLE + "friends "
-            + PREFIX_ROLE + "owesMoney "
             + PREFIX_METADATA + "I have food allegeries - Egg and Peanuts "
             + PREFIX_CCA + "NUS Cycling";
 
